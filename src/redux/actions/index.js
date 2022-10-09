e3ae16e7e7e66f6b009f8025343b8fc70117be4f
João H <joaoheldermartins@hotmail.com>
2022-10-09 10:38:18 -0300

    requisito 6

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,10 +3,11 @@ export const NOME = 'NOME';
 export const GRAVATAR = 'GRAVATAR';
 export const REQUEST_API = 'REQUEST_API';
 export const GET_TOKEN = 'GET_TOKEN';
+export const SAVE_GAME = 'SAVE_GAME';
 
 export const requestAPI = () => ({ type: REQUEST_API });
 
-export const getCurrencies = (payload) => ({ type: GET_TOKEN, payload });
+export const getToken = (payload) => ({ type: GET_TOKEN, payload });
 
 export const fetchAPI = () => async (dispatch) => {
   dispatch(requestAPI());
@@ -14,7 +15,7 @@ export const fetchAPI = () => async (dispatch) => {
     const response = await fetch('https://opentdb.com/api_token.php?command=request');
     const result = await response.json();
     localStorage.setItem('token', (result.token));
-    dispatch(getCurrencies(result.token));
+    dispatch(getToken(result));
   } catch (e) {
     throw new Error(e);
   }
