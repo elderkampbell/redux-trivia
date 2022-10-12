a4f2334c7dfd40eaeed13b27cc9138e45af2732a
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-10-12 11:28:15 -0300

    Loading estilizado

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import gif from '../assets/gif.gif';
+import '../styles/loading.css';
 
 class Loading extends Component {
   render() {
     return (
-      <h1>Loading</h1>
+      <section className="loading-section">
+        <img src={ gif } alt="gif do pikachu" />
+        <h1>LOADING...</h1>
+      </section>
     );
   }
 }
